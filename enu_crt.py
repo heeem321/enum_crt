b1f5ddbe83a23272c81ace23e307d01c8753871c
Ebrahim. Alhussaini <heeem321@hotmail.com>
2023-03-30 21:06:01 +0800

    Update enu_crt.py
    Added sort and strip www. from domains

--- enu_crt.py
+++ enu_crt.py
@@ -16,19 +16,25 @@ def parse_crt(data):
         subdomains.add(item["name_value"])
     return subdomains
 
-def main():
-    data = get_crt(domain)
-    subdomains = parse_crt(data)
-    for subdomain in subdomains:
-        print(subdomain)
+def strip_www_and_sort(subdomains):
+    subdomains = [subdomain.replace("*.","") for subdomain in subdomains]
+    subdomains = [subdomain.replace("www.","") for subdomain in subdomains]
+    subdomains = [subdomain.replace(" ","") for subdomain in subdomains]
+    subdomains = list(set(subdomains))
+    subdomains.sort()
+    return subdomains
 
-if __name__ == "__main__":
+
+def main():
     parser = argparse.ArgumentParser(description="Get subdomains from crt.sh")
     parser.add_argument("-d", "--domain", help="Domain name")
     args = parser.parse_args()
     domain = args.domain
     data = get_crt(domain)
     subdomains = parse_crt(data)
+    subdomains = strip_www_and_sort(subdomains)
     for subdomain in subdomains:
         print(subdomain)
 
+if __name__ == "__main__":
+    main()
